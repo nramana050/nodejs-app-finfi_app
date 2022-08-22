38974966f448c6894763eb99c15319d07d5e5521
Arunraj Selvam <arunraj@nstore.in>
2022-08-22 11:59:56 +0530

    org code instead of id - patch 1

--- components/TabComponent.vue
+++ components/TabComponent.vue
@@ -3,7 +3,7 @@
     div.grid.text-center(@click="navToDashboard")
       outline-collection-icon.w-6.h-8.mx-auto
       p Dashboard
-    div.grid.text-center(@click="navToCard" v-if="Number(this.organization) === 2")
+    div.grid.text-center(@click="navToCard" v-if="this.organization.code === 'NSTORE'")
       outline-credit-card-icon.w-6.h-8.mx-auto
       p Cards
     div.grid.text-center(@click="navToTransaction")
