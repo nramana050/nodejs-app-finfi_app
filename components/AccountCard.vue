beb17fbebe074165af429c5e91e4126b0a57d49b
Arunraj Selvam <arunraj@nstore.in>
2022-08-29 21:25:52 +0530

    credit_method icon message fix

--- components/AccountCard.vue
+++ components/AccountCard.vue
@@ -79,13 +79,17 @@ export default {
       }
       const fundCycle = response.data.message.toUpperCase() === 'SUCCESS' ? true : null
       if(fundCycle){
-        this.cardFundCycle = response.data.data.filter(x=>x.account_type.toUpperCase()==='CARD')
-        if(this.cardFundCycle[0].credit_method.toUpperCase() ==="PERCENTAGE"){
-          this.cardFundCycle[0].credit_value = parseFloat(this.cardFundCycle[0].salary).toFixed(2) * (parseFloat(this.cardFundCycle[0].credit_value).toFixed(2)/100) 
+        if (this.cardFundCycle.length > 0) {
+          this.cardFundCycle = response.data.data.filter(x=>x.account_type.toUpperCase()==='CARD')
+          if(this.cardFundCycle[0].credit_method.toUpperCase() ==="PERCENTAGE"){
+            this.cardFundCycle[0].credit_value = parseFloat(this.cardFundCycle[0].salary).toFixed(2) * (parseFloat(this.cardFundCycle[0].credit_value).toFixed(2)/100) 
+          }
         }
-        this.cashFundCycle = response.data.data.filter(x=>x.account_type.toUpperCase()==='CASH')
-        if(this.cashFundCycle[0].credit_method.toUpperCase()==="PERCENTAGE"){
-          this.cashFundCycle[0].credit_value = parseFloat(this.cashFundCycle[0].salary).toFixed(2) * (parseFloat(this.cashFundCycle[0].credit_value).toFixed(2)/100) 
+        if (this.cashFundCycle.length > 0) {
+          this.cashFundCycle = response.data.data.filter(x=>x.account_type.toUpperCase()==='CASH')
+          if(this.cashFundCycle[0].credit_method.toUpperCase()==="PERCENTAGE"){
+            this.cashFundCycle[0].credit_value = parseFloat(this.cashFundCycle[0].salary).toFixed(2) * (parseFloat(this.cashFundCycle[0].credit_value).toFixed(2)/100) 
+          }
         }
       }
     },
