1eb6fe4135ff83e1f1f3f8f1bc6dc9632b35f6f9
anubhavdevv <guptaanubhav907@gmail.com>
2022-10-29 00:45:18 +0530

    Remove some unwanted files

--- pages/RequestPhysical.vue
+++ pages/RequestPhysical.vue
@@ -1,8 +1,7 @@
 <template lang="pug">
 div.flex.flex-col.justify-center
-  //- div.text-center.font-bold(v-if="isCardAvailable") Card Details 
   div.text-center.font-bold Request physical card
-  FormulateForm(v-model="form" @submit="dash") 
+  FormulateForm(v-model="form") 
     div.flex.flex-cpl
       FormulateInput.pb-2.pr-3.w-full(type="text" name="address_line_1" validation="required" placeholder="Address Line 1")
     div.flex.flex-cpl
@@ -18,28 +17,24 @@ div.flex.flex-col.justify-center
     div.text-center.font-bold Card cost is ₹200 including delivery charges     
     div.flex-1.pr-4
       div.flex.flex-row.py-4.justify-center
-        button.btn.h-8.px-4.text-white.justify-center.rounded.font-bold(type="submit")(@click="requestPhysicalCard()")
-          | Pay and Submit
+        button.btn.h-8.px-4.text-white.justify-center.rounded.font-bold(@click="pay()")
+          | Pay and Submit       
 </template>
+
+<script type="application/javascript" src="https://checkout.razorpay.com/v1/checkout.js"></script>
 
 <script>
 export default {
-
-  // data() {
-  //   return {
-  //     user: this.$auth.user,
-  //     token: this.$auth.strategy.token.get(),
-  //     form: {
-  //       first_name: this.$auth.user.first_name,
-  //       last_name: this.$auth.user.last_name,
-  //       mobile: this.$auth.user.mobile,
-  //       email: this.$auth.user.email,
-  //       otp: '',
-  //       gender: '',
-  //       dob: '',
-  //     },
-  //   }
-  // },
+  head: {
+    script: [
+      {
+        type: 'text/javascript',
+        src: 'https://checkout.razorpay.com/v1/checkout.js',
+        async: true,
+        body: true,
+      }, 
+    ],
+  },
   name: 'RequestPhysical',
   data() {
     return {
@@ -50,90 +45,85 @@ export default {
         address_line_3: '',
         city: '',
         state: '',
-        pincode: null,
+        pincode: '',
       },
       isSameAddress: true,
-      script: `https://checkout.razorpay.com/v1/checkout.js`,
-      // states: {},
-      // getAmount: 0,
-      // monthlyDeposit: 0,
     }
   },
-  methods: {
-    dash(){
-      console.log(this.form)
-      // this.requestPhysicalCard()
-      // this.pay()
-      // alert('Submit Successfully')
-      // .then(response => alert('Submitted!'))
-      // .catch(function (error) {  
-      //   // console.log(error);
-      // })
 
-      // console.log(this.form)
-    },
+  methods: {
     async requestPhysicalCard() {
-      try{
-        const response = await this.$axios.$post('/m2p/requestPhysicalCard', this.form,{
-          headers: {
-            Authorization: this.token,
-          },
-        })
-        console.log("This is error")
-        console.log(response)
-        // console.log(requestPhysicalapi)
-        // if (requestPhysicalapi.length > 0) {
-        //   this.form = requestPhysicalapi[0]
-        // }
-        this.$toast.success('Payed Successfully')
-      }
-      catch (err) {
+      try {
+        const response = await this.$axios.$post('/m2p/requestPhysicalCard',this.form,
+          {
+            headers: {
+              Authorization: this.token,
+            },
+          }
+        )
+        if(response.result.result == true){
+          this.$toast.success('Physical Card Request is success')
+        }
+        if(response.result.result == false && response.result.exception.errorCode == 'Y3261'){
+          this.$toast.success('Card is registered for physical card')
+        }
+      } catch (err) {
         this.$toast.error('Failed')
       }
     },
+
+    //Razorpay payment window screen
+
     async pay() {
-      await this.$axios.post("/razarpay/payment") 
-        .then(res => {
-          const order = res.data;
-          const options = {
-            key: "rzp_test_5rnw5Qdn7jV2Pf",
-            amount: order.amount,
-            currency: 'INR',
-            description: "For Card Payment",
-            order_id: order.id,
-            prefill: {
-              name: order.name,
-              email: order.email,
-              contact: order.phone
-            },
-            theme: {
-              color: "#3399cc" // Set your website theme color
-            },
-            // handler: response => {
-            //   this.verifySignature(response)
-            // }
-          };
-
-          const rzp = new window.Razorpay(options);
-          rzp.open();
-        })
-        .catch(err => {
-          console.error(err);
-        });
-//         if(res.status == true){
-// this.requestPhysicalCard();
-//         }
-
+      const data = {
+        order: {
+          amount: 20000,
+          description: 'this is for payment',
+        },
+      }
+      await this.$axios.post('/payment/gateway/order', data).then((res) => {
+        const options = {
+          order_id: res.data.order_id,
+          currency: res.data.currency,
+          amount: res.data.amount,
+          key: res.data.key,
+          name: res.data.name,
+          description: res.data.description,
+          image: res.data.image,
+          prefill: {
+            name: res.data.prefill.name,
+            email: res.data.prefill.email,
+            contact: res.data.prefill.contact,
+          },
+          theme: {
+            color: res.data.theme.color,
+          },
+          handler: response => {
+              this.verifySignature(response);
+          }
+        }
+        const rzp1 = new Razorpay(options)
+        rzp1.open()
+      })
     },
-    
-    
 
-// requestPhys.. 
-// payment
+    //For Verify the Payment Signature
 
-
-
-
+    async verifySignature(response) {
+      await this.$axios
+        .post('/payment/gateway/verify', response)
+        .then((response) => {
+          console.log(response)
+          alert('payment received')
+          console.log(response.data.status)
+          if(response.data.status==true){
+            this.requestPhysicalCard()
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
   },
 }
 </script>
