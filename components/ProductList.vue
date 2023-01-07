f42d53c901cc79816f5ede8a96685aa59b9c1d09
anup.gogoi <anup.gogoi@berrijam.com>
2023-01-07 22:57:44 +0530

    Product list change

--- components/ProductList.vue
+++ components/ProductList.vue
@@ -1,13 +1,13 @@
 <template lang="pug">
   div
     div.ps-5
-      div.ps-2(v-for="item in this.productList" :key="item.id" @click="navToStartPlan(); selectProduct(item)")
+      div.ps-2(v-for="item in this.productList" :key="item.product.id" @click="navToStartPlan(); selectProduct(item)")
         div.ps-3.justify-center
-          img.ps-5.h-20.w-50(:src='item.product_image')
+          img.ps-5.h-20.w-50(:src='item.product.product_image')
         div.ps-4
-          div.text-xs.ps-4A.font-bold {{item.product_name}}
-          div.font-bold.text-sm.ps-4B Save for {{item.product_name}} products
-          div.text-sm Bonus reward Upto {{item.merchant_discount}}%
+          div.text-xs.ps-4A.font-bold {{item.product.product_name}}
+          div.font-bold.text-sm.ps-4B Save for {{item.product.product_name}} products
+          div.text-sm Bonus reward Upto {{item.product.merchant_discount}}%
       div.flex.ps-4.items-center.justify-center(v-if="!productList.length") No Products Found for Selected Category      
 
 </template>
@@ -54,6 +54,7 @@ export default {
   border-radius: 15px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
+
 .ps-3 {
   height: 9rem;
   border: 1px solid black;
@@ -62,18 +63,22 @@ export default {
   align-items: center;
   border: 1px solid #979797;
 }
+
 .ps-4 {
   height: 5rem;
   text-align: left;
   padding: 5px;
   margin-left: 1rem;
 }
+
 .ps-4A {
   color: #979797;
 }
+
 .ps-4B {
   padding: 5px;
 }
+
 .ps-5 {
   margin-bottom: 4rem;
 }
