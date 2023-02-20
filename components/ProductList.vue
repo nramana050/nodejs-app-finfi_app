74a2ac05b5a98a876bab51b580367c40056e5962
vaibhav katariya <vaibhav@myfinfi.com>
2023-02-20 16:09:11 +0000

    Development

--- components/ProductList.vue
+++ components/ProductList.vue
@@ -7,10 +7,17 @@
           img.custom-img(v-else :src='baseUrl+selectedCategory.category_image' crossorigin="anonymous")
         div.ps-4
           div.font-bold.text-sm.ps-4B {{item.product.product_name}}
-          div.text-sm.ps-4B Discount of {{item.product.merchant_discount}}%
+          div.text-sm.ps-4B.custom-info-container 
+           img.custom-icon(src='~/assets/discount.svg')
+           span Discount of {{item.product.merchant_discount}}%
           div.text-sm.ps-4B.custom-product-info
-           span.font-bold *{{item.product.acceptance_mode === "BOTH"? 'ONLINE & OFFLINE' : item.product.acceptance_mode }} 
-           span *Valid till {{item.product.validity}}
+          div.custom-info-container
+           img.custom-icon(src='~/assets/rupee.svg')
+           span {{item.product.acceptance_mode === "BOTH"? 'Online & Store Purchase' : item.product.acceptance_mode === "OFFLINE"? 'Store Purchase': 'Online' }} 
+          div.custom-info-container.custom-f-r 
+           img.custom-icon(src='~/assets/validity.svg')
+           span Valid till {{item.product.validity}}
+
       div.flex.ps-4.items-center.justify-center(v-if="!productList.length") No Products Found for Selected Category      
 
 </template>
@@ -54,17 +61,30 @@ export default {
 }
 </script>
 <style scoped>
+.custom-icon {
+  height: 15px;
+  width: 15px;
+  margin: 0;
+  margin-right: 5px !important;
+}
 .custom-product-info {
-  display: flex;
-  justify-content: space-between;
   font-size: 12px;
+}
+.custom-info-container {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 14px;
+}
+.custom-f-r {
+  float: right;
 }
 .custom-img {
   height: 120px;
 }
 .ps-2 {
-  margin: 2rem;
-  border-radius: 15px;
+  margin: 1rem;
+  border-radius: 5px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
 
@@ -72,9 +92,10 @@ export default {
   height: 9rem;
   border: 1px solid black;
   padding: 5px;
-  border-radius: 15px;
+  border-top-right-radius: 5px;
+  border-top-left-radius: 5px;
   align-items: center;
-  border: 1px solid #979797;
+  border: 1px solid #dbd8d8;
   display: flex;
 }
 .ps-3 img {
@@ -86,7 +107,8 @@ export default {
 .ps-4 {
   text-align: left;
   padding: 5px;
-  margin-left: 1rem;
+  margin-left: 0.5rem;
+  margin-right: 0.5rem;
 }
 
 .ps-4A {
