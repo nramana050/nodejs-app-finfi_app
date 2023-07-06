4c4169f2f7ec1bb9de58ed17c536c409cead82c3
vikasnagpal <vikas@myfinfi.com>
2023-07-06 18:22:09 +0530

    Build an UI of ADD Task page ,DailyTask Page and TaskPage

---
+++ components/Workforce_Management/TaskCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex justify-center mt-8">
+    <div style="width: 298.107px; height: 99.329px; flex-shrink: 0; background-color: #FFFFFF;">
+      <div class="m-4 flex justify-between items-center">
+        <p class="text-gray-500 text-xs font-prompt font-normal leading-normal">{{ time }}</p>
+        <a href="/TaskPage">
+          <img class="ml-auto" src="../../assets/RightIcon.svg" alt="" />
+        </a>
+      </div>
+      <p class="m-4">{{ company }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskCard',
+  props: {
+    time: {
+      type: String,
+      required: true
+    },
+    company: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+  
+</style>
+
